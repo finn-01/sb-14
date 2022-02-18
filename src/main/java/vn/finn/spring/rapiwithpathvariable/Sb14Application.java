8e3b29f940c12fa38316d250c5bd8575ce00b9d7
finn-01 <nguyensam576@gmail.com>
2022-02-18 11:46:52 +0700

    set up

---
+++ src/main/java/vn/finn/spring/rapiwithpathvariable/Sb14Application.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.rapiwithpathvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb14Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/rapiwithpathvariable/Sb14ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.rapiwithpathvariable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
